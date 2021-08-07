a4e7e48083bf7db3e201bd93a293965ba2d3c2ed
Yeab1 <46411839+Yeab1@users.noreply.github.com>
2021-08-06 21:46:29 -0500

    threading: Able to get input from both mouse and keyboard at the same time.

--- ThreadingAutomation.py
+++ ThreadingAutomation.py
@@ -4,20 +4,63 @@ import pyautogui
 from pynput.mouse import Listener 
 import pyperclip
 import threading
-clicks = []
+allActions = []
+
+def on_click(x,y, button, pressed):
+    if pressed:
+        print("Clicked: ", x, y, button)
+        if str(button) == "Button.left" or str(button) == "Button.right":
+            newClick = MouseAction("click")
+            newClick.setPosition(x, y)
+            newClick.setButton(str(button))
+            
+            allActions.append(newClick)
+        if str(button) == "Button.middle":
+            return False
+        
+def on_scroll(x,y,dx,dy):
+    newScroll = MouseAction("scroll")
+    newScroll.setPosition(x, y)
+    newScroll.setSpeed(dx, dy)
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+# ...or, in a non-blocking fashion:
+# listener = keyboard.Listener(
+#     on_press=on_press,
+#     on_release=on_release)
+# listener.start()
 
 def mouseAction():
-    pass
+    with Listener(on_click = on_click, on_scroll = on_scroll) as listener:
+        listener.join()
+from pynput import keyboard
 
 def keyBoardAction():
-    pass
+    # Collect events until released
+    with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+            listener.join()
 
 mouse = threading.Thread(target=mouseAction)
-keyboard = threading.Thread(target=keyBoardAction)
+keys = threading.Thread(target=keyBoardAction)
 
 mouse.start()
-keyboard.start()
+keys.start()
 mouse.join()
-keyboard.join()
+keys.join()
 
 print("Done")
