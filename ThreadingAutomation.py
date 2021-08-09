267702e6cfaaef20ae005cf95abac1c6e4933f8d
Yeab1 <46411839+Yeab1@users.noreply.github.com>
2021-08-08 21:37:45 -0500

    Updated program to make actions using both keyboard and mouse in addition to listening.

--- ThreadingAutomation.py
+++ ThreadingAutomation.py
@@ -8,6 +8,16 @@ from pynput import keyboard
 
 allActions = []
 
+actionDictionary = {
+    "caps_lock" : "capslock",
+    "shift" : "shiftleft",
+    "shift_r" : "shiftright",
+    "ctrl_l" : "ctrlleft",
+    "crrl_r" : "ctrlright",
+    "alt_gr" : "altright",
+    "alt_l" : "altleft"
+}
+
 def on_click(x,y, button, pressed):
     if pressed:
         print("Clicked: ", x, y, button)
@@ -67,10 +77,73 @@ keys.start()
 mouse.join()
 keys.join()
 
-print("================================Done==================================")
+print("================================ Actions ==================================")
 for action in allActions:
     if action.actionType == "click":
         print(action.getButton())
     elif action.actionType == "keyPress":
         print(str(action.getKey()))
-        # print(type(str(action.getKey())))
+print("================================ Done ==================================")
+
+# inp = int(eval(input("How many times would you like to do the action?")))
+inp = 1
+pyautogui.moveTo(allActions[0].getPosition()[0], allActions[0].getPosition()[1], duration=1)
+pyautogui.click(button= (allActions[0].getButton().split('.'))[1])
+
+for i in range(inp):
+    for index in range(1, len(allActions)):
+        
+        action = allActions[index]
+        if action.actionType == "click":
+            pyautogui.moveTo(action.getPosition()[0], action.getPosition()[1], duration=1)
+            pyautogui.click(button= (action.getButton().split('.'))[1])
+        if action.actionType == "scroll":
+            pyautogui.scroll(action.getSpeed()[1]*100, x = action.getPosition()[0], y = action.getPosition()[1])
+        if action.actionType == "keyPress":
+            # pyautogui.press('a')
+            keyString = str(action.getKey())
+            if len(keyString) > 3:
+                keyString = keyString.split('.')[1]
+            else:
+                keyString = keyString[1]
+            if keyString in actionDictionary:
+                pyautogui.press(actionDictionary[keyString], interval = 0.5)
+            else:
+                print("new string: ", keyString)
+                pyautogui.press(keyString, interval = 0.5)
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+'''          
+Testing Area
+Please test your actions here so as not to break anything
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
